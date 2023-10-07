3a0bdd5b055d514e1f2c9ba73024d52d9b89d251
yingjie <ooxx.19980@gmial.com>
2023-10-08 02:51:57 +0800

    fix

--- src/main/java/com/fubon/ecplatformapi/model/dto/vo/QueryResultVO.java
+++ src/main/java/com/fubon/ecplatformapi/model/dto/vo/QueryResultVO.java
@@ -1,16 +1,19 @@
 package com.fubon.ecplatformapi.model.dto.vo;
 
+import com.fubon.ecplatformapi.model.dto.resp.FbQueryResp;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class QueryResultVO {
-    private String insType;
-    private String policyNum;
-    private Integer premiums;
-    private String  insuredName;
-    private String plate;
-    private Date effectDate;
-    private Date expireDate;
+        private String insType;
+        private String policyNum;
+        private Integer premiums;
+        private String  insuredName;
+        private String plate;
+        private Date effectDate;
+        private Date expireDate;
+
 }
