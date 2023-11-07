264958125a48563b296dee2aaff5018ae249520b
yingjie <ooxx.19980@gmial.com>
2023-11-07 18:59:35 +0800

    修改Session

--- src/main/java/com/fubon/ecplatformapi/service/SsoService.java
+++ src/main/java/com/fubon/ecplatformapi/service/SsoService.java
@@ -1,10 +1,11 @@
 package com.fubon.ecplatformapi.service;
 
+import com.fubon.ecplatformapi.controller.SessionController;
 import com.fubon.ecplatformapi.model.dto.req.SsoReqDTO;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface SsoService {
-    String getSSOToken(String sessionId);
+public interface SsoService{
+    String getSSOToken();
     void verifySSOLogin(SsoReqDTO sspReq);
 }
